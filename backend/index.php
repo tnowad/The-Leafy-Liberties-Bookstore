3ebe022b19e0bd2ed9db3ad23d554019b1847d14
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-27 04:28:11 +0700

    add connect mysql for database

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Done with this!!";
+$host = 'database'; // The hostname or IP address of the MySQL server
+$database = 'bookstore'; // The name of the MySQL database
+$username = 'admin'; // The MySQL username
+$password = 'admin123'; // The MySQL password
+
+// Create a connection to the MySQL server
+$connection = mysqli_connect($host, $username, $password, $database);
+
+// Check if the connection was successful
+if (!$connection) {
+    die('Failed to connect to the MySQL server: ' . mysqli_connect_error());
+}
+
+echo 'Connected to the MySQL server successfully!';
+
+// Close the MySQL connection
+mysqli_close($connection);
 ?>
