da76aebfd8a87dc31081d0389969afc2b0356f30
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-28 17:02:52 +0700

    setup tailwinds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="bg-slate-500"><h1 className="text-blue-600">Start project</h1></div>
   );
 }
 
