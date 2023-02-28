ea605f56c2df8b637897f261923dd54bb2b6dbd3
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-28 22:26:32 +0700

    setup prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Done
-    </h1>
+      <h1 className="text-3xl font-bold underline">Done</h1>
     </div>
   );
 }
