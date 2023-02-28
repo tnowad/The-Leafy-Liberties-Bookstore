4248cdb28ad03ff37fb59dffd14f0215ae18fd4c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-02-28 19:05:05 +0700

    test route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,100 @@
+import * as React from "react";
+import { Routes, Route, Outlet, Link, useParams } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="bg-slate-500"><h1 className="text-blue-600">Start project</h1></div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="products">
+          <Route index element={<Products />} />
+          <Route path=":id" element={<Product />} />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
+function Products() {
+  return (
+    <div>
+      <h2>Products</h2>
+      <Link to="1">Product 1</Link>
+    </div>
+  );
+}
+
+function Product() {
+  return (
+    <div>
+      <h2>Product</h2>
+      <p>Product ID: {useParams().id}</p>
+    </div>
+  );
+}
