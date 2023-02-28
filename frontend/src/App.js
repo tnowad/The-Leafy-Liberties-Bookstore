114eacc0f8a6f660a4fdd32ca1143a132d7240b2
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-01 00:41:43 +0700

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
-function App() {
+import { Routes, Route, Outlet } from 'react-router-dom'
+export default function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Done</h1>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+function Header() {
+  return <header>Header</header>
+}
+
+function Footer() {
+  return <footer>Footer</footer>
+}
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function About() {
+  return <h1>About</h1>
+}
