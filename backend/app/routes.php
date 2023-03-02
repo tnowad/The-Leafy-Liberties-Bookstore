ee7c749e495461711ac041e0e37d0b36a9bac47c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-02 19:40:46 +0700

    format

--- backend/app/routes.php
+++ backend/app/routes.php
@@ -10,18 +10,18 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    $app->options('/{routes:.*}', function (Request $request, Response $response) {
-        // CORS Pre-Flight OPTIONS Request Handler
-        return $response;
-    });
+  $app->options('/{routes:.*}', function (Request $request, Response $response) {
+    // CORS Pre-Flight OPTIONS Request Handler
+    return $response;
+  });
 
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
-    });
+  $app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello world!');
+    return $response;
+  });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
-    });
+  $app->group('/users', function (Group $group) {
+    $group->get('', ListUsersAction::class);
+    $group->get('/{id}', ViewUserAction::class);
+  });
 };
