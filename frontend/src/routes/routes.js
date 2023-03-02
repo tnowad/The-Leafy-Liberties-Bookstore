ce7db3bbf4b9e2fabdef69646ca98f9cdc1aacab
HoangGiaBao212 <hoanggiabao21222003@gmail.com>
2023-03-02 20:44:16 +0700

    fix config/route

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -5,14 +5,14 @@ import { HeaderOnly } from '~/layouts'
 
 // Pages
 import Home from '~/pages/Home'
-import Following from '~/pages/Dashboard'
+import Dashboard from '~/pages/Dashboard'
 import Search from '~/pages/Search'
 import Profile from '~/pages/Profile'
 
 // Public routes
 const publicRoutes = [
   { path: config.routes.home, component: Home },
-  { path: config.routes.following, component: Following },
+  { path: config.routes.following, component: Dashboard },
   { path: config.routes.profile, component: Profile },
   { path: config.routes.search, component: Search, layout: null },
 ]
