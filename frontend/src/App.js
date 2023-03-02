2b842efb9fdf832a0d3b441c5b8029af8c7b07ff
HoangGiaBao212 <hoanggiabao21222003@gmail.com>
2023-03-02 23:51:12 +0700

    config router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-import { BrowserRouter, Routes, Route, Outlet, Link } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+} from 'react-router-dom'
+
+import Home from '~/pages/Home'
+import Profile from '~/pages/Profile'
+import Dashboard from '~/pages/Dashboard'
+import Cart from '~/pages/Cart'
+import Checkout from '~/pages/Checkout'
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart/checkout" element={<Checkout />} />s
+      </Routes>
+    </Router>
   )
 }
