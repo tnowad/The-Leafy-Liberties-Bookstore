a7b168bef4795e2fbb4058e64fb0daaf1e1b389a
HoangGiaBao212 <hoanggiabao21222003@gmail.com>
2023-03-03 22:44:42 +0700

    code select header

--- frontend/src/layouts/components/Header/Header.js
+++ frontend/src/layouts/components/Header/Header.js
@@ -2,11 +2,16 @@ import { logo } from '../../../assets/images'
 function Header() {
   return (
     <header className="flex justify-center w-full sticky top-0 bg-white">
-      <div className="container flex justify-between">
+      <div className="mt-5 container flex justify-between">
         <img src={logo} alt="logo" className="h-20" />
         {/* button */}
-        <button className="flex items-center justify-center w-10 h-10 text-white bg-primary rounded-full">
-          test
+        <button className="flex items-center justify-center w-44 h-10 text-white bg-primary rounded-full">
+          <select className="bg-primary cursor-pointer">
+            <option value="">Categories</option>
+            <option value="">A</option>
+            <option value="">B</option>
+            <option value="">C</option>
+          </select>
         </button>
         {/* Search form */}
         <form className="flex items-center justify-center w-1/2 h-10 bg-gray-100 rounded-full">
