17b5415dc6078535fc54c716f9bace31245b2070
HoangGiaBao212 <hoanggiabao21222003@gmail.com>
2023-03-03 23:12:30 +0700

    fix icon

--- frontend/src/layouts/components/Header/Header.js
+++ frontend/src/layouts/components/Header/Header.js
@@ -1,6 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons'
-import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import {
+  faList,
+  faSearch,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { logo } from '../../../assets/images'
 
@@ -10,24 +14,30 @@ function Header() {
       <div className="mt-5 container flex justify-between">
         <img src={logo} alt="logo" className="h-20" />
         {/* button */}
-        <button className="flex items-center justify-center w-44 h-10 text-white bg-primary rounded-full">
+        <button className="flex items-center justify-around w-44 h-10 text-white bg-primary rounded-full">
+          <FontAwesomeIcon icon={faList} />
           <select className="bg-primary cursor-pointer">
             <option value="">Categories</option>
             <option value="">A</option>
             <option value="">B</option>
             <option value="">C</option>
           </select>
+          <div></div>
         </button>
         {/* Search form */}
         <form className="flex items-center justify-center w-1/2 h-10 bg-gray-100 rounded-full">
-          <input type="text" className="w-full h-full bg-transparent" />
+          <input
+            type="text"
+            className="pl-5 w-full h-full bg-transparent"
+            placeholder="Search products..."
+          />
           <button className="flex items-center justify-center w-10 h-10 text-white bg-primary rounded-full">
             <FontAwesomeIcon icon={faSearch} />
           </button>
         </form>
 
         {/* List icons */}
-        <div>
+        <div className="w-20 flex justify-around">
           <FontAwesomeIcon icon={faHeart} />
           <FontAwesomeIcon icon={faShoppingCart} />
           <FontAwesomeIcon icon={faUser} />
