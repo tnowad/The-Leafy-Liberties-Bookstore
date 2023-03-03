1001190e29395342c70eed18a97fae2f6c363d5c
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-03 15:56:31 +0700

    rename

--- frontend/src/layouts/components/Footer/Footer.js
+++ frontend/src/layouts/components/Footer/Footer.js
@@ -1,7 +1,16 @@
-export default function Footer() {
+import React from 'react'
+
+function Footer() {
   return (
     <div>
-      <h1>Footer</h1>
+      {/* <h1 className= "text-3xl">Hello</h1> */}
+      <div className="container">
+        <div className="footer-wrapper">
+          <div className="left-container">Footer</div>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Footer
