92e3c7931496754a581cff3b366c0431ada00415
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-03 23:23:02 +0700

    use component

--- frontend/src/layouts/components/Header/Header.js
+++ frontend/src/layouts/components/Header/Header.js
@@ -7,23 +7,18 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 import { logo } from '../../../assets/images'
+import ButtonPill from '../../../components/Button/ButtonPill'
 
 function Header() {
   return (
     <header className="flex justify-center w-full sticky top-0 bg-white">
-      <div className="mt-5 container flex justify-between">
+      <div className="mt-5 container flex justify-between items-center">
         <img src={logo} alt="logo" className="h-20" />
-        {/* button */}
-        <button className="flex items-center justify-around w-44 h-10 text-white bg-primary rounded-full">
+        {/* button dropdown */}
+        <ButtonPill>
           <FontAwesomeIcon icon={faList} />
-          <select className="bg-primary cursor-pointer">
-            <option value="">Categories</option>
-            <option value="">A</option>
-            <option value="">B</option>
-            <option value="">C</option>
-          </select>
-          <div></div>
-        </button>
+          <p>Categories</p>
+        </ButtonPill>
         {/* Search form */}
         <form className="flex items-center justify-center w-1/2 h-10 bg-gray-100 rounded-full">
           <input
