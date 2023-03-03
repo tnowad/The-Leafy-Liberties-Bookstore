6b890ef5ae27a19d0705c8a1c944909057875922
PhatJack <tienphatng.693@gmail.com>
2023-03-03 14:51:35 +0700

    footer

--- frontend/src/layouts/components/footer/Footer.js
+++ frontend/src/layouts/components/footer/Footer.js
@@ -1,5 +1,18 @@
+import React from 'react'
+
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <>
+      {/* <h1 className= "text-3xl">Hello</h1> */}
+      <div className="container">
+        <div className="footer-wrapper">
+          <div className="left-container">
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Footer
