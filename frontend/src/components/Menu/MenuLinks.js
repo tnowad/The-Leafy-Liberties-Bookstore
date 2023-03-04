94110e720f372c947f41957f6d0bc2fafa40205b
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-04 23:41:58 +0700

    update viewport

--- frontend/src/components/Menu/MenuLinks.js
+++ frontend/src/components/Menu/MenuLinks.js
@@ -3,14 +3,11 @@ import { Link } from 'react-router-dom'
 export default function MenuLinks({ className, items, ...props }) {
   return (
     <ul
-      className={`flex flex-col items-center justify-center w-full h-full ${className}`}
+      className={`flex flex-col items-center w-full h-full ${className}`}
       {...props}
     >
       {items.map((item, index) => (
-        <li
-          key={index}
-          className="w-full h-12 flex items-center"
-        >
+        <li key={index} className="h-12 flex items-center">
           <Link to={item.link}>{item.name}</Link>
         </li>
       ))}
--- frontend/src/layouts/components/Footer/Footer.js
+++ frontend/src/layouts/components/Footer/Footer.js
@@ -10,13 +10,12 @@ import {
 import { bigLogo } from '../../../assets/images'
 import MenuLinks from '../../../components/Menu/MenuLinks'
 
-
 function Footer() {
   return (
     <footer className="flex justify-center w-full flex-col items-center">
       <div className="container flex">
-        <div className="left-container w-3/12 border-0 border-solid border-r-2 border-gray-400 pr-3">
-          <div className="flex">
+        <div className="left-container w-[50%] border-0 border-solid border-r-2 border-gray-400 pr-3">
+          <div className="grid grid-cols-1 lg:flex">
             <div className="w-28">
               <img src={bigLogo} alt="" />
             </div>
@@ -29,8 +28,8 @@ function Footer() {
           </div>
           <div className="description">
             <p className="text-base mt-3 p-3 font-normal">
-              LeafLiberty is a Book Store Ecommerce Website Designed by Tuan,
-              Phat ,Chinh, Bao
+              The LeafLiberty is a Book Store E-commerce Website Designed by
+              Tuan, Phat, Chinh, Bao
             </p>
           </div>
           <div className="w-full h-12 mt-6 pl-3">
@@ -42,55 +41,59 @@ function Footer() {
             </div>
           </div>
         </div>
-        <div className="right-container w-3/4 flex justify-between items-start px-10">
+        <div className="right-container w-3/4 grid grid-cols-2 sm:grid-cols-4 justify-center">
           <div className="explore">
-            <h1 className="text-xl font-medium mb-2">Explore</h1>
+            <h1 className="text-xl text-center font-medium mb-2">Explore</h1>
             <MenuLinks
               className="text-gray-600"
               items={[
-                { name: 'About us', link: '\\' },
-                { name: 'Site map', link: '\\' },
-                { name: 'Bookmarks', link: '\\' },
-                { name: 'Pricing table', link: '\\' },
+                { name: 'About us', link: '/' },
+                { name: 'Site map', link: '/' },
+                { name: 'Bookmarks', link: '/' },
+                { name: 'Pricing table', link: '/' },
               ]}
             />
           </div>
           <div className="our-services">
-            <h1 className="text-xl font-medium mb-2">Our services</h1>
+            <h1 className="text-xl text-center font-medium mb-2">
+              Our services
+            </h1>
             <MenuLinks
               className="text-gray-600"
               items={[
-                { name: 'Help center', link: '\\' },
-                { name: 'Product recalls', link: '\\' },
-                { name: 'Accessibility', link: '\\' },
-                { name: 'Store pickup', link: '\\' },
-                { name: 'Membership', link: '\\' },
+                { name: 'Help center', link: '/' },
+                { name: 'Product recalls', link: '/' },
+                { name: 'Accessibility', link: '/' },
+                { name: 'Store pickup', link: '/' },
+                { name: 'Membership', link: '/' },
               ]}
             />
           </div>
           <div className="categories">
-            <h1 className="text-xl font-medium mb-2">Categories</h1>
+            <h1 className="text-xl text-center font-medium mb-2">Categories</h1>
             <MenuLinks
               className="text-gray-600"
               items={[
-                { name: 'Action', link: '\\' },
-                { name: 'Comedy', link: '\\' },
-                { name: 'Horror', link: '\\' },
-                { name: 'Kids', link: '\\' },
-                { name: 'Drama', link: '\\' },
+                { name: 'Action', link: '/' },
+                { name: 'Comedy', link: '/' },
+                { name: 'Horror', link: '/' },
+                { name: 'Kids', link: '/' },
+                { name: 'Drama', link: '/' },
               ]}
             />
           </div>
           <div className="contact">
-            <h1 className="text-xl font-medium mb-2">Get Contact</h1>
+            <h1 className="text-xl text-center font-medium mb-2">
+              Get Contact
+            </h1>
             <MenuLinks
               className="text-gray-600"
               items={[
-                { name: 'SGU University', link: '\\' },
-                { name: 'HCM City,VietNam', link: '\\' },
-                { name: '0123456789', link: '\\' },
-                { name: '0123456789', link: '\\' },
-                { name: 'sgu@edu.vn', link: '\\' },
+                { name: 'SGU University', link: '/' },
+                { name: 'HCM City,VietNam', link: '/' },
+                { name: '0123456789', link: '/' },
+                { name: '0123456789', link: '/' },
+                { name: 'sgu@edu.vn', link: '/' },
               ]}
             />
           </div>
