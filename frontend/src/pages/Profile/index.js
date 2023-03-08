768f62af47860cc1d0645111d6d58d3ecf50e6e3
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-08 10:49:33 +0700

    fix

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,35 +2,39 @@ import { avatar, overview, user, payment, order } from '../../assets/images'
 
 function Profile() {
   return (
-    <div className="h-screen flex justify-around">
+    <div className="h-auto flex justify-around">
       {/* //? information */}
-      <div className="ml-32 mt-20 w-screen flex justify-around flex-row border border-b-2 border-green-300 box-border">
-        <div className="w-3/12">
-          <div className="pb-10">
-            <img src={avatar} className="w-44" alt="avatar" />
-            <h1 className="mt-5">Hoàng Gia Bảo</h1>
+      <div className="mx-10 mt-5 md:ml-32 md:mt-20 w-screen flex justify-around flex-col md:flex-row border border-b-2 border-green-300 box-border">
+        <div className="md:w-3/12 flex md:flex-col flex-row justify-around md:justify-start">
+          <div className="pb-10 flex flex-col content-center">
+            <img src={avatar} className="w-3/6" alt="avatar" />
+            <h1 className="mt-5 w-8/12 lg:w-5/12 inline-block  lg:text-center">
+              Hoàng Gia Bảo
+            </h1>
           </div>
 
-          <div className="mt-5 flex">
-            <img src={overview} alt="overview" className="mr-5 w-7 inline" />
-            <h3>Overview</h3>
-          </div>
-          <div className="mt-5 flex">
-            <img src={user} alt="user" className="mr-5 w-5 inline" />
-            <h3>Account settings</h3>
-          </div>
-          <div className="mt-5 flex">
-            <img src={payment} alt="" className="mr-5 w-7 inline" />
-            <h3>Payments</h3>
-          </div>
-          <div className="mt-5 flex">
-            <img src={order} alt="" className="mr-5 w-6 inline" />
-            <h3>Order</h3>
+          <div>
+            <div className="mt-5 flex">
+              <img src={overview} alt="overview" className="mr-5 w-7 inline" />
+              <h3>Overview</h3>
+            </div>
+            <div className="mt-5 flex">
+              <img src={user} alt="user" className="mr-5 w-5 inline" />
+              <h3>Account settings</h3>
+            </div>
+            <div className="mt-5 flex">
+              <img src={payment} alt="" className="mr-5 w-7 inline" />
+              <h3>Payments</h3>
+            </div>
+            <div className="mt-5 flex">
+              <img src={order} alt="" className="mr-5 w-6 inline" />
+              <h3>Order</h3>
+            </div>
           </div>
         </div>
         {/* //? information detail */}
         <div className="flex-1">
-          <form className="w-3/4 flex flex-col">
+          <form className=" md:w-3/4 flex flex-col">
             {/* //? group full name */}
             <div className="flex flex-row justify-between">
               <div className="w-4/12 flex flex-col">
@@ -71,7 +75,7 @@ function Profile() {
               required
             ></input>
             <label>Gender *</label>
-            <div className="flex justify-around w-3/12">
+            <div className="flex justify-around w-2/5 md:w-3/12">
               <input
                 type="radio"
                 name="gender"
@@ -96,12 +100,12 @@ function Profile() {
             </div>
 
             <label>Birthday *</label>
-            <div className="w-2/4 flex justify-between">
+            <div className="w-3/5 md:w-2/4 flex justify-between">
               <input
                 type="number"
                 min="1"
                 max="31"
-                className="p-1 pl-6 text-2xl border border-solid border-teal-900 rounded"
+                className="p-1 pl-6 text-base md:text-2xl border border-solid border-teal-900 rounded"
               />
               <div className="p-1 border border-solid border-teal-900 rounded">
                 <span>Month</span>
@@ -114,22 +118,22 @@ function Profile() {
               </div>
             </div>
             <label className="ml-10 mt-10">Password change</label>
-            <div className="p-10 flex flex-col border border-solid border-teal-800 rounded">
-              <div className="w-3/4">
+            <div className="p-8 flex flex-col border border-solid border-teal-800 rounded">
+              <div className="lg:w-3/4">
                 <label>Current password (leave blank to leave unchanged)</label>
                 <input
                   type="text"
                   className="h-9 p-5 w-full border border-solid border-teal-900 rounded-3xl"
                 />
               </div>
-              <div className="w-3/4">
+              <div className="lg:w-3/4">
                 <label>Current password (leave blank to leave unchanged)</label>
                 <input
                   type="text"
                   className="h-9 p-5 w-full border border-solid border-teal-900 rounded-3xl"
                 />
               </div>
-              <div className="w-3/4">
+              <div className="lg:w-3/4">
                 <label>Confirm new password</label>
                 <input
                   type="text"
