50a478151c11e5b0bff32fc14720e51891a8fb69
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-10 16:03:03 +0700

    add: show routes api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/api', function () {
+  $routeCollection = Route::getRoutes();
+  echo "<table style='width:100%'>";
+  echo "<tr>";
+  echo "<td ><h4>HTTP Method</h4></td>";
+  echo "<td ><h4>Route</h4></td>";
+  echo "<td ><h4>Name</h4></td>";
+  echo "<td ><h4>Corresponding Action</h4></td>";
+  echo "<td ><h4>Middleware</h4></td>";
+  echo "<td ><h4>Prefix</h4></td>";
+  echo "</tr>";
+  foreach ($routeCollection as $value) {
+    echo "<tr>";
+    echo "<td>" . $value->methods()[0] . "</td>";
+    echo "<td>" . $value->uri() . "</td>";
+    echo "<td>" . $value->getName() . "</td>";
+    echo "<td>" . $value->getActionName() . "</td>";
+    echo "<td>" . implode(", ", $value->middleware()) . "</td>";
+    echo "<td>" . $value->getPrefix() . "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
 });
