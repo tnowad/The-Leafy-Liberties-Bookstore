c732d2a17111d3ae06455af832ff5eb1a85fdf1b
HoangGiaBao <hoanggiabao2122003@gmail.com>
2023-03-11 12:09:57 +0700

    update dashboard

--- frontend/src/components/Button/ButtonWhite.js
+++ frontend/src/components/Button/ButtonWhite.js
@@ -3,13 +3,13 @@ function ButtonWhite({ ...props }) {
     <button
       onClick={() => (window.location.href = props.link)}
       {...props}
-      className={`mt-5 pt-2 h-10 w-36 text-gray-400  ${
+      className={`mt-5 pt-2 h-10 w-40 text-gray-400  ${
         props.color === props.title ? 'text-white bg-green-800' : ''
       } rounded-md 
-      flex justify-around content-center box-border`}
+      flex justify-center content-center box-border`}
     >
       {props.children}
-      {props.title ? <span>{props.title}</span> : ''}
+      {props.title ? <span className="ml-4">{props.title}</span> : ''}
     </button>
   )
 }
