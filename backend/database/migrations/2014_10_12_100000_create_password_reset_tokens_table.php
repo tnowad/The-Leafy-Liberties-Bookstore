98602df3cd6037d893b624a50469a2ae9a5b6d60
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-12 02:09:08 +0700

    Revert "add: model user"
    This reverts commit 34a05a41ce62f599a2342b53680519f38d8c9ed4.

--- backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ backend/database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -10,9 +10,10 @@ return new class extends Migration {
    */
   public function up(): void
   {
-    Schema::create('users', function (Blueprint $table) {
-      $table->id();
-      $table->timestamps();
+    Schema::create('password_reset_tokens', function (Blueprint $table) {
+      $table->string('email')->primary();
+      $table->string('token');
+      $table->timestamp('created_at')->nullable();
     });
   }
 
@@ -21,6 +22,6 @@ return new class extends Migration {
    */
   public function down(): void
   {
-    Schema::dropIfExists('users');
+    Schema::dropIfExists('password_reset_tokens');
   }
 };
