339ea66361861c32d73fc422571a1c6cc5b1a2f3
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-11 10:44:55 +0700

    fix: auth error

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,8 +5,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
+// Group all routes that do not require authentication here
+Route::group(['middleware' => 'guest:api'], function () {
+  Route::post('/register', [AuthController::class, 'register']);
+  Route::post('/login', [AuthController::class, 'login']);
+});
+
+// Group all routes that require authentication here
 Route::group(['middleware' => 'auth:api'], function () {
   Route::get('/user', [AuthController::class, 'user']);
-  Route::post('/login', [AuthController::class, 'login']);
-  Route::post('/register', [AuthController::class, 'register']);
 });
