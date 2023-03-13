28fd1935a43726a0b880c6d9fffb3c3a854e9ff5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-14 03:01:24 +0700

    remove user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-  return $request->user();
-});
-
 Route::group(['prefix' => 'auth'], function () {
   Route::post('register', [RegisterController::class, 'register']);
 
