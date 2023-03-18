ef0409fc77337dd17f5ba7075b178f7d523eaa35
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-18 21:43:33 +0700

    fix: text in dashboard

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -8,15 +8,14 @@ function Dashboard() {
   return (
     <div className="w-full bg-neutral-100">
       <div className="flex">
-        <DashboardManager color="Products" />
+        <DashboardManager color="Dashboard" />
         <div className="mt-10 min-h-screen w-full overflow-x-scroll xl:w-4/5 xl:overflow-x-hidden p-7">
           <div className="flex justify-between">
             <h1 className="text-xl font-bold">Dashboard</h1>
             {/* toggle the visibility of the form when the button is clicked */}
             <button className="w-5 h-5 text-2xl font-semibold">+</button>
           </div>
-          <div className="mt-5">
-          </div>
+          <div className="mt-5"></div>
         </div>
       </div>
     </div>
