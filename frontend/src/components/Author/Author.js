1143c417519cfd7e8239f9fef65967bfc69b8b39
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-19 05:21:25 +0700

    docs(client): Docs about Author

--- frontend/src/components/Author/Author.js
+++ frontend/src/components/Author/Author.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { arrowIcon,author } from '../../assets/images'
-export default  function Author() {
+import { arrowIcon, author } from '../../assets/images'
+export default function Author() {
   return (
     <div className='author flex w-full justify-evenly items-center mb-4 mobile:flex-col lg:flex-row'>
       <div className="author-face">
@@ -15,3 +15,27 @@ export default  function Author() {
     </div>
   )
 }
+/*
+?Idea: Improve performance by lazy loading the author image
+!Important: Add PropTypes to validate props values
+Todo: Replace hard-coded values with dynamic values
+
+import { FiChevronRight } from 'react-icons/fi';
+import authorImage from '../../assets/images/author.jpg';
+
+export default function AuthorCard({ name, imageUrl, onClick }) {
+  return (
+    <figure className="author-card flex w-full justify-evenly items-center mb-4 mobile:flex-col lg:flex-row">
+    <img src={imageUrl || authorImage} alt={`${name} Profile`} className="author-image rounded-full" />
+    <figcaption className="author-details flex flex-col items-center text-center">
+        <h2 className="author-name font-medium text-lg">{name || 'William Graham'}</h2>
+        {onClick && (
+          <div className="arrow-icon cursor-pointer" onClick={onClick}>
+          <FiChevronRight />
+          </div>
+          )}
+          </figcaption>
+    </figure>
+    );
+  }
+*/
