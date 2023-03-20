34ea3b001e962ad5f56370a666774958c0d34dac
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-21 04:14:52 +0700

    refactor(client): Refactor App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { publicRoutes } from './routes/routes'
 import DefaultLayout from './layouts/DefaultLayout'
-
 import { Fragment } from 'react'
 
-export default function App() {
+function App() {
+  const renderRoutes = () => {
+    return publicRoutes.map((route, index) => {
+      const Page = route.component
+      const Layout =
+        route.layout === null ? Fragment : route.layout || DefaultLayout
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          element={
+            <Layout>
+              <Page />
+            </Layout>
+          }
+        />
+      )
+    })
+  }
+
   return (
-    <Router>
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          const Page = route.component
-          let Layout = DefaultLayout
-
-          if (route.layout) Layout = route.layout
-          else if (route.layout === null) Layout = Fragment
-
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          )
-        })}
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Routes>{renderRoutes()}</Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
