ac1053a8d6f334ace7e72bdd87668d2da886ffd0
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-20 19:54:45 +0700

    refactor(client): Remove shit from Hoang Gia Bao

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { productImg, star } from '../../assets/images'
 
-export default function Product(props) {
+export default function Product({ id, name, author, price, description, rate, imageUrl }) {
+  // !Important: Replace image star with component Star
   return (
     <div className="product-info w-full flex flex-col justify-center items-center p-1">
       <div className="w-56 h-full object-cover">
-        <img src={productImg} alt="" className="w-full" />
+        <img src={imageUrl || productImg} alt="" className="w-full" />
       </div>
       <div className={`product-body w-full p-1 text-lg font-medium flex flex-col justify-center items-center`}>
         <div className={`product-name`}>
-          <a href="/book_detail">My Dearest Darkest</a>
+          <a href={"/products/" + (id || 0)}>{name || "My Dearest Darkest"}</a>
         </div>
         <div className="product-rate">
           <img src={star} alt="" />
         </div>
         <div className="product-author text-sm">Enrique Wallace</div>
-        <div className="desc">{props.desc}</div>
+        <div className="desc">{description}</div>
         <div className="product-price p-0 text-primary-900 font-semibold">
           150.000VND
         </div>
