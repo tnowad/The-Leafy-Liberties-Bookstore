cd28cb02a5807e30dad77f92c630c152c1755aa1
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-20 22:35:17 +0700

    fix(client): fix layout

--- frontend/src/layouts/DashboardLayout/DashboardLayout.js
+++ frontend/src/layouts/DashboardLayout/DashboardLayout.js
@@ -2,11 +2,10 @@
 import DashboardMenu from '../DashboardComponent/DashboardMenu';
 export default function DashboardLayout({ children }) {
   return (
-
     <div className="w-full bg-neutral-100">
       <div className="grid grid-cols-[200px_auto]">
         <DashboardMenu />
-        <div className="w-full">
+        <div className="w-full min-h-screen ">
           {children}
         </div>
       </div>
