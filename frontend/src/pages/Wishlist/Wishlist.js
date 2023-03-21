f3359854cabc58fabf701d6a097d97e48104a612
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-03-21 18:34:27 +0700

    code: wishlist

--- frontend/src/pages/Wishlist/Wishlist.js
+++ frontend/src/pages/Wishlist/Wishlist.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { productData } from '../Dashboard/Data'
 import WindowSize from '../../components/WindowSize/WindowSize'
+import { faCartPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function Wishlist() {
   const arrayTitle = [
     { name: 'Products' },
     { name: 'Category' },
-    { name: 'Entered' },
-    { name: 'Remaining' },
     { name: 'Status' },
     { name: 'Amount' },
     { name: 'Action' },
@@ -30,7 +30,11 @@ function Wishlist() {
             <tr className="w-44">
               {arrayTitle.map((item) => {
                 return (
-                  <th className="px-4 py-2" key={item.name}>
+                  <th
+                    colSpan={item.name === 'Action' ? 2 : 1}
+                    className="px-4 py-2"
+                    key={item.name}
+                  >
                     {item.name}
                   </th>
                 )
@@ -43,13 +47,26 @@ function Wishlist() {
                 <tr key={item.id} className="text-center">
                   <td>{item.image}</td>
                   <td>{item.category}</td>
-                  <td>{item.entered}</td>
-                  <td>{item.remaining}</td>
                   <td>{item.status}</td>
                   <td>{item.amount}</td>
                   <td className="border px-4 py-2">
-                    <button className="bg-green-800 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                      ...
+                    <button class="relative bg-green-800 text-white font-bold py-2 px-4 rounded hover:bg-green-600 hover:text-white">
+                      <FontAwesomeIcon className="hover:" icon={faCartPlus} />
+                      <span class="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100">
+                        <span class="w-20 relative left-16 text-green-800">
+                          Add
+                        </span>
+                      </span>
+                    </button>
+                  </td>
+                  <td className="border px-4 py-2">
+                    <button class="relative bg-green-800 text-white font-bold py-2 px-4 rounded hover:bg-green-600 hover:text-white">
+                      <FontAwesomeIcon icon={faTrash} />
+                      <span class="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100">
+                        <span class="relative left-16 text-green-800">
+                          remove
+                        </span>
+                      </span>
                     </button>
                   </td>
                 </tr>
@@ -68,7 +85,17 @@ function Wishlist() {
                 <thead>
                   <tr className="flex flex-col">
                     {arrayTitle.map((item) => {
-                      return <th key={item.name}>{item.name}</th>
+                      return (
+                        <th
+                          className={
+                            item.name === 'Action' ? `h-16 pt-5` : `h-auto`
+                          }
+                          colSpan={item.name === 'Action' ? 2 : 1}
+                          key={item.name}
+                        >
+                          {item.name}
+                        </th>
+                      )
                     })}
                   </tr>
                 </thead>
@@ -76,13 +103,17 @@ function Wishlist() {
                   <tr className="text-center flex flex-col justify-around ">
                     <td>{item.image}</td>
                     <td>{item.category}</td>
-                    <td>{item.entered}</td>
-                    <td>{item.remaining}</td>
+
                     <td>{item.status}</td>
                     <td>{item.amount}</td>
                     <td className="border px-4 py-2">
-                      <button className="bg-green-800 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                        ...
+                      <button className="bg-green-800 hover:bg-green-600 text-sm text-white font-bold py-1 px-2 rounded">
+                        Add to cart
+                      </button>
+                    </td>
+                    <td className="border px-4 py-2">
+                      <button className="bg-green-800 hover:bg-green-600 text-sm text-white font-bold py-1 px-2 rounded">
+                        Remove from wishlist
                       </button>
                     </td>
                   </tr>
