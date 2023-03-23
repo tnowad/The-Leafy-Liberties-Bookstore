45955ffe1a28fc4a2465ff1a070f144c45eedf73
PhatJack <tienphatng.693@gmail.com>
2023-03-23 19:35:49 +0700

    fix table customer and product

--- frontend/src/pages/Dashboard/Products.js
+++ frontend/src/pages/Dashboard/Products.js
@@ -2,6 +2,7 @@ import DashboardMenu from '../../layouts/DashboardComponent/DashboardMenu'
 import { useState } from 'react'
 import WindowSize from '../../components/WindowSize/WindowSize'
 import { productData } from './Data'
+import { Link } from 'react-router-dom'
 function Product({ ...props }) {
   const arrayTitle = [
     { name: 'Products' },
@@ -43,88 +44,106 @@ function Product({ ...props }) {
   }
 
   return (
-    <div>
-      <div className="flex justify-between">
-        <h1 className="text-xl">Product</h1>
-        {/* toggle the visibility of the form when the button is clicked */}
-        <button
-          className="w-5 h-5 text-2xl "
-          onClick={() => setIsFormVisible(!isFormVisible)}
-        >
-          +
-        </button>
-      </div>
-      <div className="mt-5">
-        <WindowSize onSizeChange={handleSizeChange} />
-        {window.innerWidth > 500 ? (
-          <table className="w-full border-collapse">
-            <thead className="w-full bg-gray-100 rounded-sm">
-              <tr className="w-44">
-                {arrayTitle.map((item) => {
+    <div className="w-full my-0 mx-auto overflow-x-hidden">
+      <div className="mt-10 min-h-screen box-border w-full px-10 mobile:px-5">
+        <div className="flex justify-between">
+          <h1 className="text-xl font-bold">Product</h1>
+          {/* toggle the visibility of the form when the button is clicked */}
+          <button
+            className="w-5 h-5 text-2xl "
+            onClick={() => setIsFormVisible(!isFormVisible)}
+          >
+            +
+          </button>
+        </div>
+        <div className="table-product-statistics my-8 shadow-lg cursor-pointer rounded-2xl overflow-hidden bg-white">
+          <div className="relative">
+            <WindowSize onSizeChange={handleSizeChange} />
+            {window.innerWidth > 882 ? (
+              <table className="w-full text-sm text-left text-gray-500 rounded-2xl border-collapse overflow-hidden">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    {arrayTitle.map((item) => {
+                      return (
+                        <th
+                          scope="col"
+                          colSpan={item.action === 'Action' ? 2 : 1}
+                          className="px-6 py-3"
+                        >
+                          {item.name}
+                        </th>
+                      )
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {productData.map((item) => {
+                    return (
+                      <tr className="bg-white border-b hover:bg-gray-200 transition-opacity">
+                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {item.image}
+                        </td>
+                        <td class="px-6 py-3">{item.category}</td>
+                        <td class="px-6 py-3">{item.entered}</td>
+                        <td class="px-6 py-3">{item.remaining}</td>
+                        <td class="px-6 py-3">{item.status}</td>
+                        <td class="px-6 py-3">{item.amount}</td>
+                        <td class="px-6 py-4">
+                          <Link
+                            to="#"
+                            class="font-medium text-white px-4 py-2 bg-green-700 rounded-lg"
+                          >
+                            •••
+                          </Link>
+                        </td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            ) : (
+              <>
+                {productData.map((item) => {
                   return (
-                    <th className="px-4 py-2" key={item.name}>
-                      {item.name}
-                    </th>
+                    <table className="w-full text-sm text-left text-gray-500 border-collapse overflow-hidden flex flex-row justify-between border-0 border-solid border-gray-200 border-b-[1px]">
+                      <thead className="text-xs text-gray-700 uppercase">
+                        <tr className="flex flex-col">
+                          {arrayTitle.map((item) => {
+                            return (
+                              <th scope="col" className="px-5 py-[18px] w-full">
+                                {item.name}
+                              </th>
+                            )
+                          })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="bg-white flex flex-col justify-between">
+                          <td className="px-5 py-[10px] font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {item.image}
+                          </td>
+                          <td class="px-5 py-4 w-full">{item.category}</td>
+                          <td class="px-5 py-4 w-full">{item.entered}</td>
+                          <td class="px-5 py-4 w-full">{item.remaining}</td>
+                          <td class="px-5 py-4 w-full">{item.status}</td>
+                          <td class="px-5 py-4 w-full">{item.amount}</td>
+                          <td class="px-5 py-4 w-full">
+                            <Link
+                              to="#"
+                              class="font-medium text-white px-4 py-2 bg-green-700 rounded-lg"
+                            >
+                              •••
+                            </Link>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
                   )
                 })}
-              </tr>
-            </thead>
-            <tbody>
-              {productData.map((item) => {
-                return (
-                  <tr key={item.id} className="text-center">
-                    <td>{item.image}</td>
-                    <td>{item.category}</td>
-                    <td>{item.entered}</td>
-                    <td>{item.remaining}</td>
-                    <td>{item.status}</td>
-                    <td>{item.amount}</td>
-                    <td className="border px-4 py-2">
-                      <button className="bg-green-800 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                        ...
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        ) : (
-          <>
-            {productData.map((item) => {
-              return (
-                <table
-                  key={item.id}
-                  className="ml-5 mb-8 flex flex-row justify-around border-0 border-b-2 border-gray-200 border-solid "
-                >
-                  <thead>
-                    <tr className="flex flex-col">
-                      {arrayTitle.map((item) => {
-                        return <th key={item.name}>{item.name}</th>
-                      })}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="text-center flex flex-col justify-around ">
-                      <td>{item.image}</td>
-                      <td>{item.category}</td>
-                      <td>{item.entered}</td>
-                      <td>{item.remaining}</td>
-                      <td>{item.status}</td>
-                      <td>{item.amount}</td>
-                      <td className="border px-4 py-2">
-                        <button className="bg-green-800 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
-                          ...
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              )
-            })}
-          </>
-        )}
-
+              </>
+            )}
+          </div>
+        </div>
         {isFormVisible && (
           <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-gray-500 bg-opacity-75 z-10">
             <div className="bg-white p-8 rounded-md shadow-lg">
