3fb539f653941f8cec79eec37387112c57f2d747
PhatJack <tienphatng.693@gmail.com>
2023-03-24 13:35:51 +0700

    fix genres kind

--- frontend/src/components/GenresKind/GenresKind.js
+++ frontend/src/components/GenresKind/GenresKind.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { genresHorror } from '../../assets/images'
 export default function GenresKind(props) {
   return (
-    <div className={`genres-detail relative overflow-hidden rounded-3xl cursor-pointer mr-2`}>
-      <div className="img">
+    <div className={`genres-detail relative overflow-hidden rounded-3xl cursor-pointer`}>
+      <div className="img overflow-hidden w-[450px] rounded-3xl">
         <img
           src={genresHorror}
           alt=""
-          className="rounded-3xl hover:scale-150 transition-transform duration-300"
+          className="rounded-3xl hover:scale-125 transition-transform w-full h-full"
         />
       </div>
       <p className="absolute xl:top-3/4 left-10 text-white font-normal text-4xl xl:text-3xl lg:text-2xl md:text-[22px] md:top-2/3 sm:text-[30px] mobile:top-2/3">
