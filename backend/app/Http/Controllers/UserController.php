e093a380e22501daf4f2b8315dcb7bc00ede1585
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-30 10:49:33 +0700

    feat(server): add filter

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,21 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
   public function __construct()
   {
-    // $this->middleware('auth:api');
-    // $this->middleware('permission:user-list|user-create|user-edit|user-soft-delete|user-delete', ['only' => ['index', 'show']]);
-    // $this->middleware('permission:user-create', ['only' => ['store']]);
-    // $this->middleware('permission:user-edit', ['only' => ['update']]);
-    // $this->middleware('permission:user-soft-delete', ['only' => ['softDelete']]);
-    // $this->middleware('permission:user-delete', ['only' => ['destroy']]);
+    $this->middleware('auth:api');
+    $this->middleware('permission:user-list|user-create|user-edit|user-soft-delete|user-delete', ['only' => ['index', 'show']]);
+    $this->middleware('permission:user-create', ['only' => ['store']]);
+    $this->middleware('permission:user-edit', ['only' => ['update']]);
+    $this->middleware('permission:user-soft-delete', ['only' => ['softDelete']]);
+    $this->middleware('permission:user-delete', ['only' => ['destroy']]);
   }
 
   public function index(Request $request): JsonResponse
@@ -31,17 +31,18 @@ class UserController extends Controller
     $sortField = $request->get('sort_field', 'id');
     $sortOrder = $request->get('sort_order', 'asc');
     // Handle filtering
-    $filter = $request->get('filter', 'name');
-    $filterValue = $request->get('filter_value', '');
+    $filter = $request->get('filter', null);
 
-    if ($filterValue) {
-      // full name search
-      if ($filter === 'name') {
-        $query->orWhere('first_name', 'like', "%$filterValue%")
-          ->orWhere('last_name', 'like', "%$filterValue%");
-      } else {
-
-        $query->where($filter, 'like', "%$filterValue%");
+    if (is_array($filter)) {
+      foreach ($filter as $key => $value) {
+        if ($value) {
+          if ($key === 'name') {
+            $query->where('first_name', 'like', "%$value%")
+              ->orWhere('last_name', 'like', "%$value%");
+          } else {
+            $query->where($key, 'like', "%$value%");
+          }
+        }
       }
     }
 
@@ -61,6 +62,54 @@ class UserController extends Controller
         'from' => ($page - 1) * $perPage + 1,
         'to' => $page * $perPage > User::count() ? User::count() : $page * $perPage
       ]
+    ], 200);
+  }
+
+  public function store(UserRequest $request): JsonResponse
+  {
+    $user = User::create($request->all());
+    $user->assignRole('user');
+
+    return response()->json([
+      'data' => $user,
+      'message' => 'User created successfully',
+    ], 201);
+  }
+
+  public function show(User $user): JsonResponse
+  {
+    return response()->json([
+      'data' => $user,
+    ], 200);
+  }
+
+  public function update(UserRequest $request, User $user): JsonResponse
+  {
+    $user->update($request->all());
+
+    return response()->json([
+      'data' => $user,
+      'message' => 'User updated successfully',
+    ], 200);
+  }
+
+  public function softDelete(User $user): JsonResponse
+  {
+    $user->delete();
+
+    return response()->json([
+      'data' => $user,
+      'message' => 'User soft deleted successfully',
+    ], 200);
+  }
+
+  public function destroy(User $user): JsonResponse
+  {
+    $user->forceDelete();
+
+    return response()->json([
+      'data' => $user,
+      'message' => 'User deleted successfully',
     ], 200);
   }
 }
