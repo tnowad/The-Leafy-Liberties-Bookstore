61c818ffe78ebd1db7832673d9437f77190d0905
Nguyen Minh Tuan <tnowad@gmail.com>
2023-03-30 13:10:17 +0700

    feat(server): Add test and check user login

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +14,6 @@ class DatabaseSeeder extends Seeder
     $this->call([
       PermissionTableSeeder::class,
       UserSeeder::class,
-      AdminSeeder::class,
     ]);
   }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::group(['prefix' => 'auth'], function () {
+Route::group([
+  'prefix' => 'auth'
+], function () {
   Route::post('register', [RegisterController::class, 'register']);
   Route::post('login', [LoginController::class, 'login']);
   Route::post('logout', [LoginController::class, 'logout']);
@@ -14,11 +16,10 @@ Route::group(['prefix' => 'auth'], function () {
 });
 
 Route::group([
-  'prefix' => 'users',
-  // 'middleware' => 'auth:api'
+  'prefix' => 'users'
 ], function () {
   Route::get('/', [UserController::class, 'index']);
-  Route::post('/', [UserController::class, 'create']);
+  Route::post('/', [UserController::class, 'store']);
   Route::get('/{id}', [UserController::class, 'show']);
   Route::put('/{id}', [UserController::class, 'update']);
   Route::delete('/{id}', [UserController::class, 'delete']);
